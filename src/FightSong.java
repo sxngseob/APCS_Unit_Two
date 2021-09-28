97f177e87f425153dcb7f8ff41161fa8035b89b8
sseob2 <leesa@ssfs.org>
2021-09-28 09:26:24 -0400

    fightsong

--- src/FightSong.java
+++ src/FightSong.java
@@ -7,13 +7,38 @@ public class FightSong {
      */
     public static void goTeam() {
         // Add any print statements you think should go in this method.
+        System.out.println("Go, team, go!");
+        System.out.println("You can do it.");
 
     }
 
     // Create any additional methods you need in this space in order to fulfill the requirements of the program
+    public static void best() {
+
+        System.out.println("You're the best,");
+        System.out.println("In the West.");
+
+    }
+
+    public static void space() {
+
+        System.out.println();
+
+    }
 
     public static void main(String[] args) {
         // No print statements should be in here, only method calls.
+        goTeam();
+        space();
+        goTeam();
+        best();
+        goTeam();
+        space();
+        goTeam();
+        best();
+        goTeam();
+        space();
+        goTeam();
 
     }
 }
