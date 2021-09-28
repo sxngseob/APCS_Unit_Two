2fa30eac8fe55e4f90208ae60cd5ca996b8fe9f7
sseob2 <leesa@ssfs.org>
2021-09-28 09:45:49 -0400

    FourRectanglePrinter, GrowSquarePrinter

--- src/FourRectanglePrinter.java
+++ src/FourRectanglePrinter.java
@@ -8,16 +8,17 @@ public class FourRectanglePrinter {
         System.out.println(box);
 
         // Use the translate method to move the box over to the right.
-
+        box.translate(7,0);
         // Print the box
-
+        System.out.println(box);
         // Use translate to move the box down
-
+        box.translate(0,13);
         // Print the box
-
+        System.out.println(box);
         // Use translate to move the box to the left
-
+        box.translate(-7,0);
         // print the box
+        System.out.println(box);
 
     }
 }
--- src/GrowSquarePrinter.java
+++ src/GrowSquarePrinter.java
@@ -5,12 +5,13 @@ public class GrowSquarePrinter {
     public static void main(String[] args) {
 
         // Create a Rectangle object with a starting x, y of (100, 100) and a width and height of 50.
-
+        Rectangle square = new Rectangle(100, 100, 50, 50);
         // print the rectangle object.
-
+        System.out.println(square);
         // Use the translate and grow methods as described in the instructions
-
+        square.grow(25,25);
+        square.translate(25,25);
         // print the rectangle object.
-
+        System.out.println(square);
     }
 }
