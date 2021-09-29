6b56c4b47868de37201aba14f1cd382356a050bc
sseob2 <leesa@ssfs.org>
2021-09-29 14:08:58 -0400

    rectanglefun

--- src/RectangleFun.java
+++ src/RectangleFun.java
@@ -22,15 +22,14 @@ public class RectangleFun {
         int height = scan.nextInt();
 
         // Create a Rectangle object using the four pieces of user input.
-        Rectangle square = new Rectangle(x,y,width,height);
+        Rectangle square = new Rectangle(x,y,width, height);
 
         // Use the appropriate methods (getWidth() and getHeight() ) to calculate the perimeter, then print it
         double perimeter = square.getWidth() * 2 + square.getHeight() * 2;
-        System.out.print("Perimeter: " + perimeter);
+        System.out.println("Perimeter = " + perimeter);
 
         // Use getX(), getY(), and setLocation() to move the Rectangle. Print the new location.
         square.setLocation(-1,-7);
-
         System.out.print("New location is (" + square.getX() + ", " +  square.getY() + ")");
     }
 }
